abacfbaa95e0bbe497fda4abd69737fe1d0f83f9
Raul Jheferson Velasquez Vega <rvelasquezvega@cifpfbmoll.eu>
2024-02-17 16:54:17 +0100

    ruta todos los articulos,BadMethodCallException

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,5 @@
 use App\Http\Controllers\Api\ArticleController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('articles', [ArticleController::class, 'index'])->name('api.v1.articles.index');
 Route::get('articles/{article}', [ArticleController::class, 'show'])->name('api.v1.articles.show');
