b22490d342630bb52401198725215cd5ff57ef6b
Raul Jheferson Velasquez Vega <rvelasquezvega@cifpfbmoll.eu>
2024-02-21 12:21:54 +0100

    Middleware para headers :pasa el test sin los headers accept

---
+++ tests/Feature/ValidateJsonApiHeadersTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\ValidateJsonApiHeader;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ValidateJsonApiHeadersTest extends TestCase
+{
+    use RefreshDatabase;
+    /** @test */
+    public function accept_header_must_be_present_in_all_requests()
+    {
+        Route::get('test_route', function () {
+            return 'OK';
+        })->middleware(ValidateJsonApiHeader::class);
+        $this->get('test_route')->assertStatus(406);
+    }
+}
