b788e2d26dd2813f3aa1ff5f59416489d29a668a
Raul Jheferson Velasquez Vega <rvelasquezvega@cifpfbmoll.eu>
2024-02-21 10:45:44 +0100

    ruta crear artículos:modificando el controlador

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Article;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ArticleResource;
-use App\Models\Article;
 use App\Http\Resources\ArticleCollection;
 
 class ArticleController extends Controller
@@ -19,5 +20,13 @@ class ArticleController extends Controller
     {
         $articles = Article::all();
         return ArticleCollection::make($articles);
+    }
+    public function create(Request $request)
+    {
+        dd($request->all());
+        Article::create([
+            'title'
+        ]);
+        return response(null, 201);
     }
 }
---
+++ tests/Feature/Articles/CreateArticleTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature\Articles;
+
+use Tests\TestCase;
+use App\Models\Article;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CreateArticleTest extends TestCase
+{
+    use RefreshDatabase;
+    /** @test */
+    public function can_create_articles()
+    {
+        $this->withoutExceptionHandling();
+        $response = $this->postJson(route('api.v1.articles.create'), [
+            'data' => [
+                'type' => 'articles',
+                'attributes' => [
+                    'title' => 'My first article',
+                    'slug' => 'my-first-article',
+                    'content' => 'content of my first article'
+                ]
+            ]
+        ]);
+        $response->assertCreated();
+        $article = Article::first();
+
+        $response->assertJson([
+            'data' => [
+                'type' => 'articles',
+                'id' => (string) $article->getRouteKey(),
+                'attributes' => [
+                    'title' => 'My first article',
+                    'slug' => 'my-first-article',
+                    'content' => 'content of my first article'
+                ],
+                'links' => [
+                    'self' => route('api.v1.articles.show', $article)
+                ]
+            ]
+        ]);
+    }
+}
