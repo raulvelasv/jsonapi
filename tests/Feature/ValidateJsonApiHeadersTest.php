707857ce028f9066e6b4a6d94e8709e63d3772fb
Raul Jheferson Velasquez Vega <rvelasquezvega@cifpfbmoll.eu>
2024-02-22 23:32:03 +0100

    mejora de codigo

--- tests/Feature/ValidateJsonApiHeadersTest.php
+++ tests/Feature/ValidateJsonApiHeadersTest.php
@@ -21,7 +21,7 @@ class ValidateJsonApiHeadersTest extends TestCase
     /** @test */
     public function accept_header_must_be_present_in_all_requests()
     {
-        $this->getJson('test_route')->assertStatus(406);
+        $this->get('test_route')->assertStatus(406);
         $this->get('test_route', [
             'accept' => 'application/vnd.api+json'
         ])->assertSuccessful();
